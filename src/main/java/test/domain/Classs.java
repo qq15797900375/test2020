5cc7723c1a1a81e0b85ddba669fcae7b043bb086
lxp <6130116229@email.ncu.edu.cn>
2020-11-27 11:36:46 +0800

    123

---
+++ src/main/java/test/domain/Classs.java
@@ -0,0 +1,10 @@
+package test.domain;
+
+public class Classs {
+    private int id;
+    private Student student;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
